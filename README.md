4c0560a450086aed8ded9bbf803df5a77068700e
NordLight <svetli_g_s@yahoo.com>
2023-04-10 14:38:26 +0300

    Coloring of output data displayed on the console
    Coloring of output data displayed on the console

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RockScissorsPaperBySvetlozavar
 This is the simple game "Rock, Scissors, Paper"
+Rock paper scissors is an intransitive hand game, usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V).
+Rock paper scissors is often used as a fair choosing method between two people, similar to coin flipping, drawing straws, or throwing dice in order to settle a dispute or make an unbiased group decision.
+A simultaneous, zero-sum game, it has three possible outcomes: a draw, a win or a loss. A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or "breaks scissors" or sometimes "blunts scissors"[4]), but will lose to one who has played paper ("paper covers rock"); a play of paper will lose to a play of scissors ("scissors cuts paper"). If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie.
+https://en.wikipedia.org/wiki/Rock_paper_scissors
+
+The program is a game that simulates the game "Rock, Paper, Scissors" between a player and a computer.
--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -1,11 +1,14 @@
 import random
+
 # Defining variables for Rock, Scissors and Paper
 rock = 'Rock'
 scissors = 'Scissors'
 paper = 'Paper'
 
 # Invite the player to choose one of three possibilities
-player_move = input("Choise [r]ock, [s]cissors, or [p]aper: ")
+
+print("\033[1;32mChose [r]ock, [s]cissors, or [p]aper: \033[0m", end='')  # + fg.rs (?!?)
+player_move = input().lower()
 
 # Turn player input into one of moves options and print it
 if player_move == "r":
@@ -15,8 +18,8 @@ elif player_move == "s":
 elif player_move == "p":
     player_move = paper
 else:
-    raise SystemExit("Invalid Input. Try again...")
-print(f"You chose {player_move}.")
+    raise SystemExit("\033[1;31mInvalid Input. Try again...\033[0m")
+print(f"\033[1;94mYou chose {player_move}.\033[0m")
 
 # Choose random Computer's move
 computer_move = random.randint(1, 3)
@@ -28,14 +31,15 @@ elif computer_move == 2:
     computer_move = scissors
 elif computer_move == 3:
     computer_move = paper
-print(f"The computer chose {computer_move}.")
+
+print(f"\033[1;95mThe computer chose {computer_move}.\033[0m")
 
 # Check and print the result
 if (player_move == rock and computer_move == scissors) or \
         (player_move == scissors and computer_move == paper) or \
         (player_move == paper and computer_move):
-    print("You win!")
+    print("\033[1;30;102m You win! \033[0m")
 elif player_move == computer_move:
-    print("Draw!")
+    print("\033[1;30;103m Draw! \033[0m")
 else:
-    print("You lose!")
+    print("\033[1;30;101m You lose! \033[0m")
